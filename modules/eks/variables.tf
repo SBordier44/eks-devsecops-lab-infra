33e7771ddfa83587ac08e715fb03caf2a672f9b9
SBordier44 <slye@nubox.fr>
2026-03-07 01:06:15 +0100

    reformulated comment for variable endpoint_public_access_cidrs in eks module

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -28,10 +28,14 @@ variable "endpoint_private_access" {
   default = true
 }
 
-// Specify a private IP address with CIDR prefix length for accessing the Kubernetes API
-// Using environment variables to override TF_VAR_endpoint_public_access_cidrs
-// ex: export TF_VAR_endpoint_public_access_cidrs="[\"$MY_IP4\",\"$MY_IP6\"]"
-// Get your IP4 address with: curl -4 -s https://ifconfig.me
+# IPv4 CIDR list allowed to reach the public Kubernetes API endpoint.
+# For this lab, the cluster is created as an IPv4 EKS cluster, so use IPv4 CIDRs only.
+#
+# Example:
+# export TF_VAR_endpoint_public_access_cidrs="[\"82.67.127.199/32\"]"
+#
+# Get your public IPv4 with:
+# curl -4 -s https://ifconfig.me
 variable "endpoint_public_access_cidrs" {
   type    = list(string)
   default = ["0.0.0.0/0"]
